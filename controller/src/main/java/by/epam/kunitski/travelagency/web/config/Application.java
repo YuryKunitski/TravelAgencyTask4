d75f7e3bfb3fe5de8038e5d081b5243942abe8ae
Yury Kunitski <yury_kunitski@epam.com>
2019-09-13 17:38:05 +0300

    try to config security

--- controller/src/main/java/by/epam/kunitski/travelagency/web/config/Application.java
+++ controller/src/main/java/by/epam/kunitski/travelagency/web/config/Application.java
@@ -1,5 +1,7 @@
 package by.epam.kunitski.travelagency.web.config;
 
+import by.epam.kunitski.travelagency.dao.entity.User;
+import by.epam.kunitski.travelagency.dao.repository.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,8 +13,8 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoRepositories("by.epam.kunitski.travelagency.dao.repository")
 public class Application implements CommandLineRunner {
 
-//    @Autowired
-//    private ReviewDAO reviewDAO;
+    @Autowired
+    private UserDAO userDAO;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -20,7 +22,9 @@ public class Application implements CommandLineRunner {
 
        @Override
     public void run(String... args) throws Exception {
-
+//        User user = userDAO.findByUsername("");
+//
+//           System.out.println( userDAO.save(user));
     }
 
 }
